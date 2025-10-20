7ae68d0b367d2c183b73b3b178d3ae54fc9a37d9
huzaifa910 <huzaifa2533@gmail.com>
2025-10-20 17:55:23 +0500

    initial commit

--- connect-mongoDB/app.js
+++ connect-mongoDB/app.js
@@ -1,42 +1,41 @@
-import express from "express"
-import mongoose from "mongoose"
-import userModel from "./models/userSchema.js"
+import express from "express";
+import mongoose from "mongoose";
+import userModel from "./models/userSchema.js";
 
-let app = express()
-let PORT = process.env.PORT || 5007
+let app = express();
+let PORT = process.env.PORT || 5007;
 
 // connect Mongo DB
-let URI = "mongodb+srv://admin:Admin321@cluster0.pbdevl9.mongodb.net/tododata?retryWrites=true&w=majority&appName=Cluster0"
+let URI =
+  "mongodb+srv://admin:Admin321@cluster0.pbdevl9.mongodb.net/tododata?retryWrites=true&w=majority&appName=Cluster0";
 mongoose
-.connect(URI)
-.then((res)=>console.log("Mongo DB connected..."))
-.catch((err)=>console.log("Mongo DB error " , err.message))
+  .connect(URI)
+  .then((res) => console.log("Mongo DB connected..."))
+  .catch((err) => console.log("Mongo DB error ", err.message));
 
-// body parser 
+// body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// create user 
-app.post("/createuser" , async(request , response)=>{
-
-try {
-    let body = request.body
-    console.log("Body" , body)
-    await userModel.create(body)    
-    
-} catch (error) {
+// create user
+app.post("/createuser", async (request, response) => {
+  try {
+    let body = request.body;
+    console.log("Body", body);
+    await userModel.create(body);
+  } catch (error) {
     response.json({
-        message: "error from create user",
-        error: error.message
-    })
-}
-})
+      message: "error from create user",
+      error: error.message,
+    });
+  }
+});
 
-app.get("/getuser" , (req ,res) => {
-      res.json({
-        message: "Response from get users",
-        data: req.body
-      })
-})
+app.get("/getuser", (req, res) => {
+  res.json({
+    message: "Response from get users",
+    data: req.body,
+  });
+});
 
-app.listen( PORT , ()=>console.log(`Server is running from port ${PORT}` ))
+app.listen(PORT, () => console.log(`Server is running from port ${PORT}`));
